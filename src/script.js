0849f5b1a779deb0ae4e09abab23d24294bb0378
pappu dutta <pappudutta279@gmail.com>
2024-08-12 09:35:47 +0530

    refactor: improve code readability and maintainability

--- src/script.js
+++ src/script.js
@@ -1,24 +1,27 @@
 // This function is an example of "event delegation" concept.
 
 function allBtnLink() {
-  document.body.addEventListener("click", openApp);
+  let projectSec = document.getElementsByClassName("project-section");
+
+  projectSec[0].addEventListener("click", openApp);
 
   function openApp(e) {
     let domain = window.location.href.split("index.html") + "projects/";
 
-    const from = e.target.parentElement.querySelector("H2");
+    let linkPathText = e.target.parentElement.querySelector("H2").innerHTML;
 
     // Suggested fixes ----
     // Instead of parentElement use closest
     // handle error
     // on image click shows error
+    // error: while clicking on the h1 gets opened stopwach - fix it
 
-    let filePath = from.innerHTML.split(" ").join("-").toLowerCase();
+    let filePath = linkPathText.split(" ").join("-").toLowerCase();
 
-    let finalURL =
-      domain.toString().split(",").join("") + filePath + "/index.html";
+    let finalURL = domain + filePath + "/index.html";
+    console.log(finalURL);
 
-    window.location.href = finalURL;
+    // window.location.href = finalURL;
   }
 }
 
