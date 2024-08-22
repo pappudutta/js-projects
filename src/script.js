7ea18cdb8527671d65bfd53a5d37655373356ed0
pappu dutta <pappudutta279@gmail.com>
2024-08-22 19:33:47 +0530

    feat: new feature added

--- src/script.js
+++ src/script.js
@@ -3,12 +3,21 @@
 function allBtnLink() {
   let projectSec = document.getElementsByClassName("project-section");
 
-  projectSec[0].addEventListener("click", openApp);
+  console.log(projectSec);
+  let arrProjectSection = Array.from(projectSec);
+  console.log(arrProjectSection);
+
+  arrProjectSection.forEach(section => {
+    section.addEventListener("click", openApp);
+  });
+
+  // projectSec[0].addEventListener("click", openApp);
 
   function openApp(e) {
     let domain =
       window.location.href.split("index.html").join("") + "projects/";
     let linkPathText = e.target.parentElement.querySelector("H2").innerHTML;
+    console.log(e);
 
     // Suggested fixes ----
     // Instead of parentElement use closest
